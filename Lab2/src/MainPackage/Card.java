f941b98542bc18222d5ac0481c3ae043018cdcd7
purplerabbit <ethangclark@gmail.com>
2014-09-22 02:07:33 -0400

    did everything... not sure what's up with the group, ha, but it's here. yeah.

--- Lab2/src/MainPackage/Card.java
+++ Lab2/src/MainPackage/Card.java
@@ -2,26 +2,21 @@ package MainPackage;
 
 public class Card {
 	
-	private Value value;
-	private Suit suit;
+	private int value;
+	private String suit;
 	
-	public Card(){
-		
-	}
-	
-	public Card(Value value, Suit suit){
+	public Card(int value, String suit){
 		this.value = value;
 		this.suit = suit;
 	}
-/*the prompt says no public setters/getters...
- * I'm going to delete these methods tomorrow if nobody objects.
- */
-	public void setValue(Value value){
-		this.value = value;
+	
+	public int getValue(){
+		return this.value;
 	}
-
-	public void setSuit(Suit suit){
-		this.suit = suit;
+	
+	public String getSuit(){
+		return this.suit;
 	}
+	
 }
 
--- Lab2/src/MainPackage/Play.java
+++ Lab2/src/MainPackage/Play.java
@@ -1,10 +1,11 @@
 package MainPackage;
 
-/*I'm going to delete the suit and value classes tomorrow unless someone objects.
- * They shouldn't be their own classes because they aren't included in the prompt.
- * The functionality they provide should be internal to Card/Deck/Hand/Play
- */
-
 public class Play {
-
+	public static void main(String[] args){
+		Deck thisDeck = new Deck();
+		
+		Hand thisHand = new Hand(thisDeck, 5);
+		
+		System.out.println("The score for this hand is " + Hand.judge(thisHand) + ".");
+	}
 }
